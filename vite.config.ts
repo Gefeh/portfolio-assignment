481498c10494d366ff60e8a7c4efa40e6d1d3656
gefeeh <bennydros@gmail.com>
2025-04-02 21:48:46 +0200

    k

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import plugin from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/portfolio-assignment/',
-    plugins: [plugin()],
+  plugins: [plugin()],
     server: {
         port: 55787,
     }
